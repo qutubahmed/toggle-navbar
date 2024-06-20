a579c216cdf58b75183d9fb224201fbfbef62bf5
qutub ahmed <qutubahmed104@gmail.com>
2024-06-20 16:09:38 +0500

    'main'

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-let nav = document.querySelector('.secondNav');
-nav.style.display = 'none';
-let image = document.createElement('img')
-image.src = 'images/sidebar.png';
-image.style.width = '20px'
-image.setAttribute('class','image')
-document.body.appendChild(image);
-image.addEventListener('click',()=>{
-    nav.style.display = 'flex';
-})
 
